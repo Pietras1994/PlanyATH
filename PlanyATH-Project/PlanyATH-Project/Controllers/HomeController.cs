15c60af99e75dadbd6303fce79bda8ea3ac28618
Pietras1994 <pieter8965@gmail.com>
2017-04-07 15:36:39 +0200

    commtest

--- PlanyATH-Project/PlanyATH-Project/Controllers/HomeController.cs
+++ PlanyATH-Project/PlanyATH-Project/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace PlanyATH_Project.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
