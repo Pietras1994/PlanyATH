05bddc2b8330e66217203fae2a880d5ba6881242
Pietras1994 <pieter8965@gmail.com>
2017-05-06 13:45:58 +0200

    Baza

--- PlanyATH-Project/PlanyATH-Project/Controllers/HomeController.cs
+++ PlanyATH-Project/PlanyATH-Project/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Db4objects.Db4o;
 
 namespace PlanyATH_Project.Controllers
 {
     public class HomeController : Controller
     {
+        IObjectContainer db;
+        public void DbInit()
+        {
+            db = Db4oEmbedded.OpenFile("DataBase.data");
+        }
+
         public ActionResult Index()
         {
             return View();
---
+++ PlanyATH-Project/PlanyATH-Project/Models/DataModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PlanyATH_Project.Models
+{
+    public enum eType
+    {
+        ClassRoom,
+        Lecturer
+    }
+    public class DataModel
+    {
+        public string Name { get; set; }
+        public eType eType { get; set; }
+        public string Link { get; set; }
+    }
+}
